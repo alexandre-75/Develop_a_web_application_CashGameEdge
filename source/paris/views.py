601f90acfc6f032609eb480e2a75fe2107727844
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-06-08 17:43:20 +0200

    display list + btn return

--- source/paris/views.py
+++ source/paris/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import ParisEtablissement
 
 def flux_paris(request):
-    return render(request, "paris/paris_flux.html")
+    
+    etablissements = ParisEtablissement.objects.all()
+    return render(request, "paris/paris_flux.html", context={"etablissements": etablissements})
